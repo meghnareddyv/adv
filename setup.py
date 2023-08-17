249b75d1c27cc9bc800095753184972acb01169f
meghnareddyv <meghanareddyv13@gmail.com>
2023-08-17 14:54:49 +1000

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='adv_mla_lab_1 at1',
+    author='Meghana',
+    license='MIT',
+)
